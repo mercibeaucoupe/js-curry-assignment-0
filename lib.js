b591dda1689c59842765553bc0d9310ff32b7ba0
unknown <jung_woo@live.com>
2017-10-26 19:47:17 -0500

    Day 9 Woo Jung

--- lib.js
+++ lib.js
@@ -29,7 +29,11 @@ const cart =
 const itemRepeater =
   itemName =>
     count => {
-      // TODO
+      var arr = []
+      for (let i = 0; i < count; i++) {
+        arr.push(itemName)
+      }
+      return arr
     }
 
 /**
@@ -39,8 +43,20 @@ const itemRepeater =
 const constructCarts =
   listings =>
     customers => {
-      // TODO
-    }
+      return customers.map(customer => ({                             // Pulls out each customer element in customers
+        customer: customer.name,                                      // Sets customer's name property with the key customer:
+          items: ( entries(customer.shoppingList)                     // entries(obj) sets shoppingList item's key's to it's value
+            .reduce((acc, next) =>                                    // E.g. [ ['item name' : quantity/count], ... ]
+              [...acc, ...itemRepeater(next[0])(next[1])], []))       // next becomes an Element of the Nested Array.
+                .reduce((acc, next) => [...acc, ...listings           // Spread operator spreads the nested arrays to create one array
+                  .filter(listing => next === listing.name) ], [])    // Currying, Index 0 holds item name, Index 1 holds count.
+      })                                                              // itemRepeater()() returns an array with JUST the item name.
+    )                                                                 // Need to replace all of the populated item names with
+}                                                                     // useable Objects. Need to grab the objects from listings.
+                                                                      // Removes listings item objects if it's NOT in shoppingList.
+                                                                      // Now has name and price property and is an Object    
+
+
 
 module.exports = {
   listing,
